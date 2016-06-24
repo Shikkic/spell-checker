5c2ab4b5c86c0eca243bdaefdfe628739620000b
John <jjkiely@gmail.com>
2016-06-24 18:25:18 +0100

    kinda hacky, mostly tested

---
+++ checker.py
@@ -0,0 +1,33 @@
+import re, collections
+
+def words(text):
+  return re.findall('[a-z]+', text.lower())
+
+def train(features):
+  model = collections.defaultdict(lambda:1)
+  for f in features:
+    model[f]+= 1
+  return model
+
+NWORDS = train(words(open('corpus.txt').read()))
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+def edit_distance_one(word):
+  splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+  deletes = [a + b[1:] for a, b in splits if b]
+  transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
+  replace = [a + c + b[1:] for a, b in splits for c in alphabet if b]
+  inserts = [a + b + b for a, b in splits for c in alphabet]
+  return known(set(deletes + transposes + replace + inserts))
+
+def known_edit_distance_two(word):
+  return set(e2 for e1 in edit_distance_one(word) for e2 in edit_distance_one(e1) if e2 in NWORDS)
+
+def known(words):
+  return set(w for w in words if w in NWORDS)
+
+def correct(word):
+  candidates = known([word]) or edit_distance_one(word) or known_edit_distance_two(word) or [word]
+
+  return max(candidates, key=NWORDS.get)
---
+++ test_checker.py
@@ -0,0 +1,25 @@
+import pytest
+import checker
+
+def test_words():
+  s = 'Hello World!'
+  words = checker.words(s)
+  assert words == ["hello", "world"]
+
+def test_train():
+  l = ['hello', 'world']
+  model = checker.train(l)
+  assert model['hello'] == 2
+  assert model['world'] == 2
+  assert model['moon'] == 1
+  assert model['hey'] == 1
+
+def test_edit_one():
+  edits = {'aat', 'act', 'at', 'bat', 'ca', 'caa', 'cab', 'cac', 'cad', 'cae', 'caf', 'cag', 'cah', 'cai', 'caj', 'cak', 'cal', 'cam', 'can', 'cao', 'cap', 'caq', 'car', 'cas', 'cat', 'catat', 'catcat', 'catt', 'cau', 'cav', 'caw', 'cax', 'cay', 'caz', 'cbt', 'cct', 'cdt', 'cet', 'cft', 'cgt', 'cht', 'cit', 'cjt', 'ckt', 'clt', 'cmt', 'cnt', 'cot', 'cpt', 'cqt', 'crt', 'cst', 'ct', 'cta', 'ctt', 'cut', 'cvt', 'cwt', 'cxt', 'cyt', 'czt', 'dat', 'eat', 'fat', 'gat', 'hat', 'iat', 'jat', 'kat', 'lat', 'mat', 'nat', 'oat', 'pat', 'qat', 'rat', 'sat', 'tat', 'uat', 'vat', 'wat', 'xat', 'yat', 'zat'}
+  assert checker.edit_distance_one('cat') == edits
+
+def test_known_edit_two():
+  assert len(checker.known_edit_distance_two('cat')) == 149
+
+def test_known():
+  assert checker.known(['dog','cat','wereafasfasdf']) == {'dog','cat'}
