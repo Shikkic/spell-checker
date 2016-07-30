172ecde51b3e0956f6fe46d811dd0f55964b5e9e
John <jjkiely@gmail.com>
2016-07-30 15:57:52 -0400

    changed NWORDS to word_count

--- lib/checker.py
+++ lib/checker.py
@@ -2,9 +2,8 @@ import re, collections, os
 
 class Checker:
 
-  def __init__(self):
-    
-    self.NWORDS = self.train(self.words(open(os.path.join('data', 'corpus.txt')).read()))
+  def __init__(self):    
+    self.word_count = self.train(self.words(open(os.path.join('data', 'corpus.txt')).read()))
     self.alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
   def bigrams(self, text):
@@ -40,11 +39,11 @@ class Checker:
     return (self.known(self.edit_distance_one(word)))
 
   def known_edit_distance_two(self, word):
-    return set(e2 for e1 in self.edit_distance_one(word) for e2 in self.edit_distance_one(e1) if e2 in self.NWORDS)
+    return set(e2 for e1 in self.edit_distance_one(word) for e2 in self.edit_distance_one(e1) if e2 in self.word_count)
 
   def known(self, words):
-    return set(w for w in words if w in self.NWORDS)
+    return set(w for w in words if w in self.word_count)
 
   def correct(self, word):
     candidates = self.known([word]) or self.known_edit_distance_one(word) or self.known_edit_distance_two(word) or [word]
-    return max(candidates, key=self.NWORDS.get)
+    return max(candidates, key=self.word_count.get)
