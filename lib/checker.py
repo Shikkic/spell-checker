d697af1e7000e8f9716acc99cd6fbcb784272669
John <jjkiely@gmail.com>
2016-07-31 17:00:26 -0400

    split trainer and checker into differnt files, also fixed calculate function

--- lib/checker.py
+++ lib/checker.py
@@ -1,70 +1,14 @@
-import re, collections, os
+from .trainer import Trainer
 from pyxdameraulevenshtein import damerau_levenshtein_distance
 
 class Checker:
-
-  def __init__(self, corpus='corpus.txt'):
-    self.corpus = open(os.path.join('data', corpus)).read()
-    self.alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
-  def train(self):
-    word_list = self.words(self.corpus)
-    bigram_list = self.bigrams(self.corpus)
-    trigram_list = self.trigrams(self.corpus)
-    self.word_count = self.train_model(word_list)
-    self.bigram_count = self.train_model(bigram_list)
-    self.trigram_count = self.train_model(trigram_list)
-    self.unigram_probs = self.get_probs(self.word_count)
-    self.bigram_probs = self.get_probs(self.bigram_count)
-    self.trigram_probs = self.get_probs(self.trigram_count)
-
-  def get_probs(self, count):
-    prob_dict = collections.defaultdict(lambda:1)
-    denom = sum(count.values())
-    for gram in count:
-      prob_dict[gram] = (count[gram]/denom)
-    return prob_dict
-
-  def bigrams(self, text):
-    l = []
-    lines = filter(None, re.split('[.?1\n]+', text))
-    for line in lines:
-      mod_line = ["^"] + self.words(line) + ["$"]
-      for i in range(len(mod_line) - 1):
-        l.append((mod_line[i], mod_line[i+1]))
-    return l
-
-  def trigrams(self, text):
-    l = []
-    lines = filter(None, re.split('[.?1\n]+', text))
-    for line in lines:
-      mod_line = ["^"] + self.words(line) + ["$"]
-      for i in range(len(mod_line) - 2):
-        l.append((mod_line[i], mod_line[i+1], mod_line[i+2]))
-    return l
-      
-  def words(self, text):
-    return re.findall('[a-z\']+', text.lower())
-
-  def train_model(self, features):
-    model = collections.defaultdict(lambda:1)
-    for f in features:
-      model[f]+= 1
-    return model
-
-  def edit_distance_one(self, word):
-    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
-    deletes = [a + b[1:] for a, b in splits if b]
-    transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
-    replace = [a + c + b[1:] for a, b in splits for c in self.alphabet if b]
-    inserts = [a + b + b for a, b in splits for c in self.alphabet]
-    return set(deletes + transposes + replace + inserts)
-
-  def known_edit_distance_one(self, word):
-    return (self.knowns(self.edit_distance_one(word)))
-
-  def known_edit_distance_two(self, word):
-    return set(e2 for e1 in self.edit_distance_one(word) for e2 in self.edit_distance_one(e1) if e2 in self.word_count)
+  def __init__(self, trainer=Trainer):
+    trainer = Trainer()
+    data = trainer.data
+    self.word_count = data['word_count']
+    self.unigram_probs = data['unigram_probs']
+    self.bigram_probs = data['bigram_probs']
+    self.trigram_probs = data['trigram_probs']
 
   def knowns(self, words):
     return set(w for w in words if w in self.word_count)
@@ -72,16 +16,10 @@ class Checker:
   def is_known(self, word):
     return word in self.word_count
 
-  def correct(self, word):
-    candidates = self.knowns([word]) or self.known_edit_distance_one(word) or self.known_edit_distance_two(word) or [word]
-    return max(candidates, key=self.word_count.get)
+  def words(self, text):
+    return re.findall('[a-z\']+', text.lower())
 
   def check_sentence(self, sentence):
-    #for each incorrect word:
-    #  for each known word:
-    #    poss += (correction, (unigram prob)*(bigram prob)*(trigram prob)*(error prob))
-    #    some kind of cut off?
-    #return top five possibilities
     sentence_list = ["^"] + self.words(sentence) + ["$"]
     corrections_list = []
     for i, word in enumerate(sentence_list):
@@ -104,21 +42,18 @@ class Checker:
         self.error_prob(word, poss))
       rl.append((poss, prob))
     rl.sort(key=lambda tup: tup[1])
+    rl.reverse()
     return rl[:5]
           
-
   def unigram_prob(self, word):
-    #prob = (self.word_count[word]/sum(self.word_count.values()))
     prob = self.unigram_probs[word]
     return prob
 
   def bigram_prob(self, bigram):
-    #prob = (self.bigram_count[bigram]/sum(self.bigram_count.values()))
     prob = self.bigram_probs[bigram]
     return prob
 
   def trigram_prob(self, trigram):
-    #prob = (self.trigram_count[trigram]/sum(self.trigram_count.values()))
     prob = self.trigram_probs[trigram]
     return prob
 
--- tests/test_checker.py
+++ tests/test_checker.py
@@ -2,87 +2,12 @@ import pytest
 from ..lib.checker import Checker
 
 checker = Checker()
-checker.train()
-
-def test_words():
-  s = 'Hello World!'
-  words = checker.words(s)
-  assert words == ["hello", "world"]
-
-def test_bigram():
-  s = "Hello World!, What's up?"
-  bigrams = checker.bigrams(s)
-  assert bigrams == [("^", "hello"),("hello", "world"), ("world", "what's"), ("what's", "up"), ("up", "$")]
-
-def test_trigrams():
-  s = "The quick brown fox"
-  trigrams = checker.trigrams(s)
-  assert trigrams == [("^", "the", "quick"), ("the","quick","brown"), ("quick","brown","fox"), ("brown", "fox", "$")]
-
-def test_train_model():
-  l = ['hello', 'world']
-  model = checker.train_model(l)
-  assert model['hello'] == 2
-  assert model['world'] == 2
-  assert model['moon'] == 1
-  assert model['hey'] == 1
-
-def test_train_bigrams():
-  s = "Hello World!, What's up?"
-  l = checker.bigrams(s)
-  model = checker.train_model(l)
-  assert model[("^", "hello")] == 2
-  assert model[("april", "moon")] == 1
-
-def test_train_trigrams():
-  s = "The quick brown fox"
-  l = checker.trigrams(s)
-  model = checker.train_model(l)
-  assert model[("^", "the", "quick")] == 2
-  assert model[("a", "b", "c")] == 1
-
-def test_edit_one():
-  edits = {'aat', 'act', 'at', 'bat', 'ca', 'caa', 'cab', 'cac', 'cad', 'cae', 'caf', 'cag', 'cah', 'cai', 'caj', 'cak', 'cal', 'cam', 'can', 'cao', 'cap', 'caq', 'car', 'cas', 'cat', 'catat', 'catcat', 'catt', 'cau', 'cav', 'caw', 'cax', 'cay', 'caz', 'cbt', 'cct', 'cdt', 'cet', 'cft', 'cgt', 'cht', 'cit', 'cjt', 'ckt', 'clt', 'cmt', 'cnt', 'cot', 'cpt', 'cqt', 'crt', 'cst', 'ct', 'cta', 'ctt', 'cut', 'cvt', 'cwt', 'cxt', 'cyt', 'czt', 'dat', 'eat', 'fat', 'gat', 'hat', 'iat', 'jat', 'kat', 'lat', 'mat', 'nat', 'oat', 'pat', 'qat', 'rat', 'sat', 'tat', 'uat', 'vat', 'wat', 'xat', 'yat', 'zat'}
-  assert checker.edit_distance_one('cat') == edits
-
-def test_known_edit_one():
-  assert len(checker.known_edit_distance_one('cat')) == 25
-  
-def test_known_edit_two():
-  assert len(checker.known_edit_distance_two('cat')) == 236
 
 def test_knowns():
   assert checker.knowns(['dog','cat','wereafasfasdf']) == {'dog','cat'}
 
 def test_is_known():
   assert checker.is_known('dog') == True
-
-def test_correct_knowns():
-  assert checker.correct('chair') == 'chair'
-
-def test_correct_dist_one():
-  assert checker.correct('chiar') == 'chair'
-
-def test_correct_dist_two():
-  assert checker.correct('chaire') == 'chair'
-
-def test_correct_unknown():
-  assert checker.correct('asdfasghaguhapefhsahuapsas') == 'asdfasghaguhapefhsahuapsas'
-
-def test_unigram_prob():
-  wds = checker.words("echo bravo fox echo golf")
-  checker.word_count = checker.train_model(wds)
-  assert checker.unigram_prob('echo') == (3/9)
-
-def test_bigram_prob():
-  bgms = checker.bigrams("echo bravo fox echo bravo golf")
-  checker.bigram_count = checker.train_model(bgms)
-  assert checker.bigram_prob(("echo", "bravo")) == (3/13)
-
-def test_trigram_prob():
-  trgms = checker.trigrams("echo bravo fox echo bravo fox golf")
-  checker.trigram_count = checker.train_model(trgms)
-  assert checker.trigram_prob(("echo", "bravo", "fox")) == (3/13)
 
 def test_error_prob():
   assert checker.error_prob('chair', 'chaire') == (1/2)
