8946b44ec1debda3caed86c2b27a6f426de874db
John <jjkiely@gmail.com>
2016-07-31 18:43:16 -0400

    updated evalutate, import problems

--- tests/evaluate.py
+++ tests/evaluate.py
@@ -1,6 +1,6 @@
 import time
-from test_errors import unigram_one, unigram_two
-from checker import Checker
+from errors import unigram_one, unigram_two
+from ..lib.checker import Checker
 
 checker = Checker()
 
@@ -8,15 +8,20 @@ checker = Checker()
 def evaluator(tests, bias=None):
   n, bad, unknown, start = 0, 0, 0, time.clock()
   if bias:
-    for target in tests: checker.NWORDS[target] += bias
+    for target in tests: checker.word_count[target] += bias
   for target, wrongs in tests.items():
     for wrong in wrongs.split():
       n+= 1
-      w = checker.correct(wrong)
-      if w != target:
+      trys = get_ans(wrong)
+      if target not in words:
         bad += 1
-        unknown += (target not in checker.NWORDS)
+        unknown += (target not in checker.word_count)
   return dict(bad=bad, n=n, bias=bias, pct=int(100. -100.*bad/n), unknown=unknown, secs=int(time.clock() - start))
 
 print(evaluator(unigram_one))
-print(evaluator(unigram_two))
+#print(evaluator(unigram_two))
+
+def get_ans(word):
+  result = checker.check_sentence(word)
+  words = [w[0] for w in result[0]]
+  return words
