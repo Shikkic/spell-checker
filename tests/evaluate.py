a51026926b980a85b79a639dd165cc821a19b195
John <jjkiely@gmail.com>
2016-08-03 21:12:35 -0400

    added false neg to evaluate

--- tests/evaluate.py
+++ tests/evaluate.py
@@ -8,18 +8,18 @@ from lib.checker import Checker
 checker = Checker()
 
 
-def evaluator(tests, bias=None):
-  n, bad, unknown, start = 0, 0, 0, time.clock()
-  if bias:
-    for target in tests: checker.word_count[target] += bias
+def evaluator(tests):
+  n, bad, unknown, start, false_neg = 0, 0, 0, time.clock(), 0
   for target, wrongs in tests.items():
     for wrong in wrongs.split():
       n+= 1
       trys = get_ans(wrong)
+      if trys == []:
+        false_neg += 1
       if target not in trys:
         bad += 1
         unknown += (target not in checker.word_count)
-  return dict(bad=bad, n=n, bias=bias, pct=int(100. -100.*bad/n), unknown=unknown, secs=int(time.clock() - start))
+  return dict(bad=bad, n=n, false_neg=false_neg,pct=int(100. -100.*bad/n), unknown=unknown, secs=int(time.clock() - start))
 
 what = []
 def get_ans(word):
@@ -28,10 +28,8 @@ def get_ans(word):
     words = [w[0] for w in result[0]]
     return words
   except IndexError:
-    what.append(word)
     return []
 
 
 print(evaluator(unigram_one))
-print(what)
 #print(evaluator(unigram_two))
